79aef5aabd2ccf5ddb8936c6d8ee13fd1a663b3f
Brea Brennan <bbrennan21@morbius.mscsnet.mu.edu>
2020-09-14 21:33:15 -0500

    Added to testcase.c file

--- xinu-hw3/system/testcases.c
+++ xinu-hw3/system/testcases.c
@@ -24,41 +24,59 @@ devcall putc(int dev, char c)
  */
 void testcases(void)
 {
-    uchar c = '0';
-
-   // kputc('h');
+    uchar c;
 
     kprintf("\n===TEST BEGIN===\r\n");
-    
-    //Test 1: unget a character and check to see if its avalible in the buffer array
-    kungetc('X');
-    if(kcheckc())
-    {
-	kprintf("Character available\n");
-    }
-    else
-    {
-	kprintf("Didn't work\n");
-    }
 
-    //Test 2: Get a character
     c = kgetc();
     switch (c)
     {
-	case 'X':
-		kprintf("The character was retrieved from the unbuffer array.\n");
+	//This test case should unget the character 'X' and then print it to the screen using after getting the character via kgetc
+	case '1':
+		kprintf("Test Case 1\r\n");
+		kungetc('X');
+		c = kgetc();
+		kprintf("Character = %c\r\n", c);		
 		break;
-
+	//This test case should check to see if there is a currently avalaible character to retrieve either in the UART register or in the ungetArray
+	case '2':
+		kprintf("Test Case 2\r\n");
+		if(kcheckc())
+		{
+			kprintf("Character avaliable\r\n");
+		}
+		else
+		{
+			kprintf("No character avalaible to check.\r\n");
+		}
+		break;
+	//This test case should put the character 'h' onto the terminal window
+	case '3':
+		kprintf("Test Case 3\r\n");
+		kputc('h');
+		break;
+	//
+	case '4':
+		kprintf("Test Case 4\r\n");
+		c = kgetc(); //wait for input via the command line
+		kprintf("The character entered was : %c\r\n", c);
+	//Testing more than one character input via command line
+	case '5':
+		kprintf("Test Case 5\r\n");
+		kungetc('B');
+		if(kcheckc()){
+			kprintf("Working\r\n");
+		}
+		else{
+			kprintf("Not working\r\n");
+		}
+		break;
+		
         // TODO: Test your operating system!
 
     default:
-        kprintf("Hello Xinu World!\r\n");
+        kprintf("Character = %c\r\n", c);
     }
-
-    //Test 3: putting a statement into the UART and/or Buffer array, then getting it and printing the statment to the screen 
-    kputc('B');
-    c = kgetc(); 
-    kprintf("C = %c\n", c);
 
     kprintf("\r\n===TEST END===\r\n");
     return;
