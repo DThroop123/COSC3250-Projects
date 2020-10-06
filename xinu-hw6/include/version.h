c6ad51e1ed95ae0a81bed8d221bcec1c7a9d9b40
Daniel Throop <dthroop@morbius.mscsnet.mu.edu>
2020-10-06 09:47:41 -0500

    Added preemption state

--- xinu-hw6/include/version.h
+++ xinu-hw6/include/version.h
@@ -1,1 +1,1 @@
-#define VERSION "(Embedded Xinu) (arm-rpi3) #164 (bbrennan21@morbius) Mon Oct 5 12:30:35 CDT 2020"
+#define VERSION "(Embedded Xinu) (arm-rpi3) #177 (dthroop@morbius) Mon Oct 5 21:15:45 CDT 2020"
