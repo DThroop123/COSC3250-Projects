fad3b26bbd73e3a7730fe4a2712006d54dae47cd
Brea Brennan <bbrennan21@morbius.mscsnet.mu.edu>
2020-10-22 12:44:53 -0500

    Changing the parameters for the create.c function

--- xinu-hw6/system/create.c
+++ xinu-hw6/system/create.c
@@ -28,7 +28,7 @@ void *getstk(ulong);
  * @param nargs    number of arguments that follow
  * @return the new process id
  */
-syscall create(void *funcaddr, ulong ssize, char *name, uint tickets, ulong nargs, ...)
+syscall create(void *funcaddr, ulong ssize, uint tickets, char *name, ulong nargs, ...)
 {
     ulong *saddr;               /* stack address                */
     ulong pid;                  /* stores new process id        */
