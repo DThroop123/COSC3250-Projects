ca0ab3aac05c058b6a5becbdc112805062e58c37
Daniel Throop <dthroop@morbius.mscsnet.mu.edu>
2020-11-06 19:40:09 -0600

    Completed lock functions

--- xinu-hw9/system/pthread.c
+++ xinu-hw9/system/pthread.c
@@ -7,18 +7,33 @@ pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_rout
 
 pthread_join(pthread_t thread, void **retval);
 {
+
+
 }
 
 syscall pthread_mutex_lock(pthread_mutex_t *mutex)
 {
+	lock_acquire(&mutex);
 }
 
 syscall pthread_mutex_trylock(pthread_mutex_t *mutex)
 {
+	
+	irqmask im;
+   	im = disable();
+  	while (FALSE == _atomic_compareAndSwapWeak(&mutex,
+                                                 LOCK_UNLOCKED,
+                                                 LOCK_LOCKED | (im & ARM_I_BIT)))
+  	{                           // do nothing while cas fails
+   	}
+
+   	return OK;
+
 }
 
 syscall pthread_mutex_unlock(pthread_mutex_t *mutex)
 {
+	lock_release(&mutex);
 }
 
 
